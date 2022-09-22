d0a44480d9abd37f2c5cdfd491f623c54e3276a8
KrispyChiggen <100893257+KrispyChiggen@users.noreply.github.com>
2022-09-22 18:04:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $(".table").prepend($("<thead></thead>").append($(this).find("tr:first"))).DataTable();
+        });
+    </script>
     <style>
         .wrapper{
             width: 600px;
